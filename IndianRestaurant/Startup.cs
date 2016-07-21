10d71fccb691b55923c3c6dc6f67ecd2ea60c2c5
Ritesh Patel <patelritesh261@outlook.com>
2016-07-21 09:04:42 -0400

    Add project files.

---
+++ IndianRestaurant/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IndianRestaurant.Startup))]
+namespace IndianRestaurant
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
